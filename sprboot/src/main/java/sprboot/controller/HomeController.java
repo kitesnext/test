62e21f5e3b960fd6f4de73ac94528255a40a6644
kitesnext <32243373+kitesnext@users.noreply.github.com>
2019-01-28 19:35:12 +0200

    1commit

---
+++ sprboot/src/main/java/sprboot/controller/HomeController.java
@@ -0,0 +1,11 @@
+package sprboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String getIndex() {
+        return "index";
+}
+}
